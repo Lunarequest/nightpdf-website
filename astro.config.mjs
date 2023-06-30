594d4c125227936ca872e0fc18d3743297519f43
Luna <luna.dragon@suse.com>
2023-06-30 10:53:29 +0530

    switch to pnp loader in yarn and use astro-icon@next for easy performance wins

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,23 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
-import { searchForWorkspaceRoot } from "vite";
 import image from "@astrojs/image";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://nightpdf.nullrequest.com",
-	integrations: [sitemap(), tailwind(), react(), image()],
+	integrations: [
+		sitemap(),
+		tailwind(),
+		icon({
+			include: {
+				mdi: ["*"],
+			},
+		}),
+		react(),
+		image(),
+	],
 	output: "server",
 	adapter: vercel(),
 });
